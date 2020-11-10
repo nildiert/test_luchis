e2cd72bab34bc00cfe6b0f65999ed1cc6a096b2e
nildiert <niljordan23@gmail.com>
2020-11-10 13:30:55 -0500

    [FEATURE] Added counter effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,4 +171,22 @@
     items: 1
   });
 
+  $('#ready').waypoint(function() {
+    $('.counter-count').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 5000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+  }, {
+    offset: '100%'
+  });
+
+
+});
+
 })(jQuery);
